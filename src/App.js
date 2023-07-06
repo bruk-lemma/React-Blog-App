3b2d9cc60091bd129c8d4f2b1b4d9ac8ac1025d3
bruk-lemma <bruklemma2017@gmail.com>
2023-07-06 15:25:15 +0300

    created a custome context provider for the whole blog app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 import { useContext } from "react";
+import { PostProvider, PostContext } from "./PostContext";
 
 function createRandomPost() {
   return {
@@ -9,33 +10,8 @@ function createRandomPost() {
   };
 }
 
-//1) Create A Context
-const PostContext = createContext();
-
 function App() {
-  const [posts, setPosts] = useState(() =>
-    Array.from({ length: 30 }, () => createRandomPost())
-  );
-  const [searchQuery, setSearchQuery] = useState("");
   const [isFakeDark, setIsFakeDark] = useState(false);
-
-  // Derived state. These are the posts that will actually be displayed
-  const searchedPosts =
-    searchQuery.length > 0
-      ? posts.filter((post) =>
-          `${post.title} ${post.body}`
-            .toLowerCase()
-            .includes(searchQuery.toLowerCase())
-        )
-      : posts;
-
-  function handleAddPost(post) {
-    setPosts((posts) => [post, ...posts]);
-  }
-
-  function handleClearPosts() {
-    setPosts([]);
-  }
 
   // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
   useEffect(
@@ -47,15 +23,7 @@ function App() {
 
   return (
     //2) Provide  a  value to child component
-    <PostContext.Provider
-      value={{
-        posts: searchedPosts,
-        onAddPost: handleAddPost,
-        onClearPost: handleClearPosts,
-        searchQuery,
-        setSearchQuery,
-      }}
-    >
+    <PostProvider>
       <section>
         <button
           onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
@@ -69,7 +37,7 @@ function App() {
         <Archive />
         <Footer />
       </section>
-    </PostContext.Provider>
+    </PostProvider>
   );
 }
 
